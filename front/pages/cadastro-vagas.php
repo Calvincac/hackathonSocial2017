fce286e62cd41713c12897788301f8571d97a537
Robson Lopes <robson.97.lopes@gmail.com>
2017-03-26 12:23:30 -0300

    Implementação

--- front/pages/cadastro-vagas.php
+++ front/pages/cadastro-vagas.php
@@ -3,67 +3,51 @@
 ?>
 
 <main class="main cadastrar-vaga">
-  
+
   <?php include('../partials/topo-empresa.php'); ?>
 
   <div class="container">
     <div class="content">
       <h1 class="text-center">Cadastrar nova vaga</h1>
-      <form class="form">
-        
+      <form class="form" id="form-vagas">
+
         <div class="form-group">
-          <label for="carga">Cargo</label>
-          <input type="text" class="form-control">
+          <label for="cargo">Cargo</label>
+          <input type="text" id="cargo" name="cargo" class="form-control">
         </div>
-        
+
         <div class="form-group">
           <label for="description">Descrição da vaga*</label>
-          <textarea name="description" class="form-control"></textarea>
+          <textarea name="descricao" class="form-control"></textarea>
         </div>
 
         <div class="form-group">
           <label for="description">Requisitos*</label>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Requisito 1">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Requisito 2">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Requisito 3">
-          </div>
+          <input type="text" class="form-control" name="habilidades_necessarias">
         </div>
 
         <div class="form-group col-md-6 no-padding-left">
-          <label for="faixa">Faixa salarial:*</label>
-          <input type="text" class="form-control">
+          <label for="faixa">Salário:*</label>
+          <input type="text" name="faixa_salarial" class="form-control">
         </div>
 
         <div class="form-group col-md-6 no-padding-right">
           <label for="carga">Carga horária:*</label>
-          <input type="text" class="form-control">
+          <input type="text" name="carga_horaria" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="local">Local:*</label>
-          <input type="text" class="form-control">
+          <input type="text" name="localizacao" class="form-control">
         </div>
 
         <div class="form-group">
           <label for="description">Benefícios</label>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Benefício 1">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Benefício 2">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Benefício 3">
-          </div>
+          <input type="text" class="form-control" name="beneficios">
         </div>
 
         <div class="text-center">
-          <button type="submit" class="btn-action btn btn-default btn-lg"> 
+          <button type="submit" class="btn-action btn btn-default btn-lg">
             Salvar
           </button>
         </div>
@@ -72,7 +56,8 @@
     </div>
   </div>
 </main>
-
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="../js/cadastro-vaga.js"></script>
 <?php
   include('../partials/footer.php');
 ?>
