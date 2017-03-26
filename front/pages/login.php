e1f95882e225cdd3d9d6ef9071b8660c6e3efd28
Vinicius Farias <vinifdelfino@hotmail.com>
2017-03-26 05:37:16 -0300

    estilizando página de login

--- front/pages/login.php
+++ front/pages/login.php
@@ -2,16 +2,32 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Hackathon</title>
+    <title>Tua Chance</title>
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
+    <header class="header-login">
+      <div class="container">
+        <div class="box-logo">
+          <img src="../images/logo.png" alt="" class="img-responsive">
+        </div>
+      </div>
+    </header><!-- /header -->
     <div class="login">
+      <div class="banner">
+        <img src="../images/login.jpg" alt="login" class="img-responsive">
+      </div>
       <div class="cont">
-        <figure class="logo">
-          <img src="//placehold.it/250x100" alt="Logo">
-        </figure>
+        <h1 class="title">
+          LOGIN
+        </h1>
         <form>
           <div class="form-group">
             <input type="text" class="form-control" placeholder="E-mail">
