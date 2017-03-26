3de46cebe57dee9702f70266aeae5f0a71a8add1
Vinicius Farias <vinifdelfino@hotmail.com>
2017-03-26 00:02:02 -0300

    listagens

---
+++ front/pages/listagem-vagas.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hackathon</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <div class="top">
+      <div class="container">
+        <div class="flex-top">
+          <img src="//placehold.it/120x70" alt="Logo">
+          <!-- <a href="#" class="btn btn-default">Sair</a> -->
+        </div>
+      </div>
+    </div>
+    <div class="topo-admin">
+      <div class="jumbotron">
+        <div class="banner">
+          <div class="image">
+            <img src="//placehold.it/1920x200" alt="" class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <aside class="pull-left pull-left col-md-3">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <a href="listagem-alunos.php" title="Listar alunos">
+              Alunos
+            </a>
+          </li>
+          <li class="list-group-item">
+            <a href="listagem-empresas.php" title="Listar empresas">
+              Empresas
+            </a>
+          </li>
+          <li class="list-group-item">
+            <a href="listagem-vagas.php" title="Listar vagas">
+              Vagas
+            </a>
+          </li>
+        </ul>
+      </aside>
+      <div class="content col-md-9">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>Nome da vaga</th>
+              <th>Editar</th>
+              <th>Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nome da vaga</td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Editar</a></td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Excluir</a></td>
+            </tr>
+            <tr>
+              <td>Nome da vaga</td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Editar</a></td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Excluir</a></td>
+            </tr>
+            <tr>
+              <td>Nome da vaga</td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Editar</a></td>
+              <td><a href="javascript: void(0)" class="btn btn-default" title="">Excluir</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
