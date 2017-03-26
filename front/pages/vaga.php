c06951850dd1e23676ce331cf54b79398816c58c
Robson Lopes <robson.97.lopes@gmail.com>
2017-03-26 03:51:48 -0300

    Estrutura da pagina vagas

--- front/pages/vaga.php
+++ front/pages/vaga.php
@@ -1,11 +1,38 @@
-<?php 
+<?php
   include('../partials/header.php');
 ?>
 
-<main>
-  
+<main class="main vaga">
+  <div class="container">
+    <div class="content">
+      <h1>Auxiliar de mecânica júnior</h1>
+      <div class="row">
+        <div class="col-xs-12 col-md-8">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div class="col-xs-12 col-md-4">
+          <ul class="list-skills">
+            <li>
+              <i class="fa fa-money"></i>
+              <span><strong>Faixa salarial:</strong> R$800,00 - R$ 1.200,00</span>
+            </li>
+            <li>
+              <i class="fa fa-clock-o"></i>
+              <span><strong>Carga horária:</strong> 40h semanais</span>
+            </li>
+            <li>
+              <i class="fa fa-globe"></i>
+              <span><strong>Localização:</strong> Porto Alegre - RS</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 
-<?php 
+<?php
   include('../partials/footer.php');
 ?>
