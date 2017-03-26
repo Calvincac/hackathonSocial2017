8eb8b2f567187b6c885ca1cb5055723c8065fd5f
Vinicius Farias <vinifdelfino@hotmail.com>
2017-03-25 23:45:59 -0300

    implementando layout básico do login e listagem de alunos

---
+++ front/pages/listagem-alunos.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hackathon</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <div class="top">
+      <div class="container">
+        <div class="flex-top">
+          <img src="//placehold.it/120x70" alt="Logo">
+          <!-- <a href="#" class="btn btn-default">Sair</a> -->
+        </div>
+      </div>
+    </div>
+    <div class="topo-admin">
+      <div class="jumbotron">
+        <div class="banner">
+          <div class="image">
+            <img src="//placehold.it/1920x200" alt="" class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <aside class="pull-left col-md-4">
+        <ul class="list-group">
+          <li class="list-group-item">
+            <a href="listagem-alunos.php" title="Listar alunos">
+              Alunos
+            </a>
+          </li>
+          <li class="list-group-item">
+            <a href="listagem-empresas.php" title="Listar empresas">
+              Empresas
+            </a>
+          </li>
+          <li class="list-group-item">
+            <a href="listagem-vagas.php" title="Listar vagas">
+              Vagas
+            </a>
+          </li>
+        </ul>
+      </aside>
+      <div class="content col-md-offset-1">
+        
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
+
--- front/pages/login.php
+++ front/pages/login.php
@@ -11,13 +11,36 @@
       <div class="container">
         <div class="flex-top">
           <img src="//placehold.it/120x70" alt="Logo">
-          <a href="#" class="btn btn-default">Sair</a>
+          <!-- <a href="#" class="btn btn-default">Sair</a> -->
         </div>
       </div>
     </div>
 
     <div class="content">
-
+      <div class="container">
+        <form action="#" method="post" class="col-xs-12 col-md-6 col-md-offset-3">
+          <h1 class="text-center">Login</h1>
+          <div class="form-group">
+            <label for="email-login" class="">
+              Email
+            </label>
+            <input class="form-control" type="email" name="email"></input>
+          </div>
+          <div class="form-group">
+            <label for="password-login">
+              Senha
+            </label>
+            <input type="password" name="password" class="form-control">
+          </div>
+          <div class="form-group field-submit">
+            <a href="listagem-alunos.php"  class="btn btn-primary">Entrar</button>
+            <a href="#" class="btn btn-default pull-right">Cadastrar empresa</a>
+          </div>
+          <div class="form-group text-center">
+            <a href="#" title="">Esqueceu sua senha?</a>
+          </div>
+        </form>
+      </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
