b38cab7b068a331c816ca33e9a8317f0f8298a3a
Vinicius Farias <vinifdelfino@hotmail.com>
2017-03-26 03:18:43 -0300

    chamando css do sistema

--- front/pages/home-empresa.php
+++ front/pages/home-empresa.php
@@ -2,8 +2,36 @@
   include('../partials/header.php');
 ?>
 
-<main class="main">
+<main class="home-empresa">
   <?php include('../partials/topo-empresa'); ?>
+
+  <div class="content-page">
+    <div class="topo-home-empresa">
+      <h2 class="title-page">Vagas</h2>
+
+      <a href="#" title="Cadastrar página" class="btn btn-primary pull-right">
+        Cadastra vaga
+      </a>
+    </div>
+
+    <div class="list-vagas">
+      <div class="item-vaga">
+        <div class="main-infos">
+          <h3>Nome da vaga</h3>
+          <div class="description">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </div>
+          <div class="qtd-indicados">
+            
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </main>
 
 <?php 
