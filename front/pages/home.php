7def4f40d994a26994c845e051a2fdf333b8c63c
Robson Lopes <robson.97.lopes@gmail.com>
2017-03-25 23:31:59 -0300

    Adicionado header

--- front/pages/home.php
+++ front/pages/home.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <title>Hackathon</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-    <div class="top">
+    <header class="header">
       <div class="container">
         <div class="flex-top">
           <img src="//placehold.it/120x70/000/fff" alt="Logo">
@@ -25,9 +26,9 @@
           <a href="#" class="btn btn-default">Acessar</a>
         </div>
       </div>
-    </div>
+    </header>
 
-    <div class="content home">
+    <main class="content home">
       <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
         <ol class="carousel-indicators">
@@ -66,7 +67,36 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
-    </div>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="flex-footer">
+          <ul class="footer-itens">
+            <li>
+              <i class="fa fa-phone"></i>
+              <a href="#">51 0000 0000</a> \ <a href="#">51 0000 0000</a>
+            </li>
+            <li>
+              <i class="fa fa-envelope"></i>
+              <a href="#">institucional@paodospobres.com.br</a>
+            </li>
+          </ul>
+          <ul class="footer-redes">
+            <li>
+              <a href="#" class="fb">
+                <i class="fa fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#" class="yt">
+                <i class="fa fa-youtube"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
