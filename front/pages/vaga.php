f1eee23665b4504924edd27a438f1a5259a526fb
Robson Lopes <robson.97.lopes@gmail.com>
2017-03-26 11:26:36 -0300

    pagina de vaga

--- front/pages/vaga.php
+++ front/pages/vaga.php
@@ -2,7 +2,7 @@
   include('../partials/header.php');
 ?>
 
-<main class="main vaga">
+<main class="main vaga" id="vaga" v-model="vaga">
   <?php include('../partials/topo-empresa.php'); ?>
 
   <div class="container">
@@ -10,9 +10,9 @@
       <div class="row">
         <div class="flex-cols">
           <div class="col-xs-12 col-md-8">
-            <h1>Auxiliar de mecânica júnior</h1>
+            <h1>{{ vaga.cargo }}</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              {{ vaga.descricao }}
             </p>
             <h3>Benefícios</h3>
             <ul class="list-itens">
@@ -23,17 +23,18 @@
           </div>
           <div class="col-xs-12 col-md-4">
             <ul class="list-skills">
-              <li>
-                <i class="fa fa-money"></i>
-                <span><strong>Faixa salarial:</strong> R$800,00 - R$ 1.200,00</span>
+              <li class="money">
+                <span>
+                  Salário <strong>{{ vaga.faixa_salarial | currency }}</strong>
+                </span>
               </li>
               <li>
                 <i class="fa fa-clock-o"></i>
-                <span><strong>Carga horária:</strong> 40h semanais</span>
+                <span>40h semanais</span>
               </li>
               <li>
-                <i class="fa fa-globe"></i>
-                <span><strong>Localização:</strong> Porto Alegre - RS</span>
+                <i class="fa fa-map-marker"></i>
+                <span>Porto Alegre - RS</span>
               </li>
             </ul>
           </div>
@@ -126,3 +127,4 @@
 <?php
   include('../partials/footer.php');
 ?>
+<script src="../js/vaga.js"></script>
--- front/partials/footer.php
+++ front/partials/footer.php
@@ -4,7 +4,6 @@
 
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.5/vue.min.js"></script>
-    <script src="../js/home-empresa.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
